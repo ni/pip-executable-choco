52469a22483673c034fb11ef1d5996534b310e53
Matthew Shafer 🐍 <matthew.shafer@ni.com>
2023-08-01 10:29:17 -0500

    remove non-essential comments for uninstall
    Signed-off-by: Matthew Shafer <matthew.shafer@ni.com>

--- pip_executable_choco/tools/chocolateyuninstall.ps1
+++ pip_executable_choco/tools/chocolateyuninstall.ps1
@@ -1,23 +1,8 @@
-﻿# IMPORTANT: Before releasing this package, copy/paste the next 2 lines into PowerShell to remove all comments from this file:
-#   $f='c:\path\to\thisFile.ps1'
-#   gc $f | ? {$_ -notmatch "^\s*#"} | % {$_ -replace '(^.*?)\s*?[^``]#.*','$1'} | Out-File $f+".~" -en utf8; mv -fo $f+".~" $f
+﻿
 
-## NOTE: In 80-90% of the cases (95% with licensed versions due to Package Synchronizer and other enhancements),
-## AutoUninstaller should be able to detect and handle registry uninstalls without a chocolateyUninstall.ps1.
-## See https://docs.chocolatey.org/en-us/choco/commands/uninstall
-## and https://docs.chocolatey.org/en-us/create/functions/uninstall-chocolateypackage
 
-## If this is an MSI, ensure 'softwareName' is appropriate, then clean up comments and you are done.
-## If this is an exe, change fileType, silentArgs, and validExitCodes
+$ErrorActionPreference = 'Stop';
 
-$ErrorActionPreference = 'Stop'; # stop on all errors
-
-## OTHER POWERSHELL FUNCTIONS
-## https://docs.chocolatey.org/en-us/create/functions
-#Uninstall-ChocolateyZipPackage $packageName # Only necessary if you did not unpack to package directory - see https://docs.chocolatey.org/en-us/create/functions/uninstall-chocolateyzippackage
-#Uninstall-ChocolateyEnvironmentVariable # 0.9.10+ - https://docs.chocolatey.org/en-us/create/functions/uninstall-chocolateyenvironmentvariable
-#Uninstall-BinFile # Only needed if you used Install-BinFile - see https://docs.chocolatey.org/en-us/create/functions/uninstall-binfile
-## Remove any shortcuts you added in the install script.
 
 Write-Host "Warning: Only removing pipx and path entry. Not removing pipx installed venvs. These can be found under $env:UserProfile\.local\pipx"
 
